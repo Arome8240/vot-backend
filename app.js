b4abeb8fdf663f2174080d627a398e390b157113
Dev Arome <Moses21429@gmail.com>
2022-02-23 15:15:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
+const fileupload = require('express-fileupload')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
+
+const errorHandler = require('./middleware/error')
+
+const DBConnection = require('./config/db')
+
+dotenv.config({ path: './config/.env' })
+
+DBConnection()
+
+const authRoutes = require('./routes/auth')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// File uploading
+app.use(
+  fileupload({
+    createParentPath: true
+  })
+)
+
+// Sanitize data
+app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Enable CORS
+app.use(cors())
+
+// Prevent http param pollution
+app.use(hpp())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', (req, res) => {
+    res.send('Hello, I\'m back')
+})
+
+const versionOne = (routeName) => `/api/v1/${routeName}`
+
+//app.use(`/api/v1/auth`, authRoutes)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+
+const server = app.listen(PORT, () => {
+  console.log(
+    `We are live on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+})
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
